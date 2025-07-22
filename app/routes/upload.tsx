31ae09dbed0ec786a2b3e1316e23f4fdad903309
Anas Khan <annaasskhan6@gmail.com>
2025-07-22 23:56:51 +0530

    Building Analyzer Function

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,14 +1,30 @@
 import {type FormEvent, useState} from 'react'
 import Navbar from "~/components/Navbar";
 import FileUploader from "~/components/FileUploader";
+import {usePuterStore} from "~/lib/puter";
+import {useNavigate} from "react-router";
 
 const Upload = () => {
+    const { auth, isLoading, fs, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
     const [file, setFile] = useState<File | null>(null)
 
     const handleFileSelect = (file: File | null) => {
           setFile(file)
+    }
+
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file }: { companyName: string, jobTitle: string, jobDescription: string, file: File  }) => {
+        setIsProcessing(true);
+        setStatusText('Uploading the file...');
+        const uploadedFile = await fs.upload([file]);
+        if(!uploadedFile) return setStatusText('Error: Failed to upload file');
+
+        setStatusText('Converting to image...');
+        // const imageFile = await convertPdfToImage(file);
+        // if(!imageFile.file) return setStatusText('Error: Failed to convert PDF to image');
+        //
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -21,8 +37,10 @@ const Upload = () => {
         const jobTitle = formData.get('job-title') as string;
         const jobDescription = formData.get('job-description') as string;
 
+        if(!file) return;
 
-       console.log(companyName, jobTitle, jobDescription);
+        handleAnalyze({ companyName, jobTitle, jobDescription, file });
+
     }
 
     return (
